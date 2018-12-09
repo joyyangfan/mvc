43aadf445d4417dba573fe77d151e15ca85c4f21
yangfan <tcjoyyangfan@163.com>
2018-12-09 09:48:35 +0800

    1

--- MyMvc.Web/Program.cs
+++ MyMvc.Web/Program.cs
@@ -20,6 +20,6 @@ namespace MyMvc.Web
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://*:4000");
+                .UseUrls("http://*:5000");
     }
 }
