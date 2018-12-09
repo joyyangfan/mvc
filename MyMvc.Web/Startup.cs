0b9803972598c81cd286d14524c6993d644cad9e
joyyangfan <374391006@qq.com>
2018-12-09 13:03:07 +0800

    Update Startup.cs

--- MyMvc.Web/Startup.cs
+++ MyMvc.Web/Startup.cs
@@ -27,6 +27,7 @@ namespace MyMvc.Web
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
+                cjcj
                 // ////This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
